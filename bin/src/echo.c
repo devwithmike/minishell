5cce05494292413ff0b0127daebe4a033fd27549
MikeFMeyer <mfmeyer301@gmail.com>
2019-08-07 16:02:54 +0200

    "echo"

--- bin/src/echo.c
+++ bin/src/echo.c
@@ -6,7 +6,7 @@
 /*   By: mimeyer <mimeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 13:37:22 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/08/07 15:47:38 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/08/07 16:01:23 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int exec_echo(char *cmd)
 {
 	char *av;
+	char *temp;
 	int i;
 
 	i = 1;
-	av = ft_strtrim(ft_strsub(cmd, 5, ft_strlen(cmd) - 5));
+	temp = ft_strsub(cmd, 5, ft_strlen(cmd) - 5);
+	av = ft_strtrim(temp);
 	ft_putendl(av);
 	free(av);
+	free(temp);
 	return (1);
 }
